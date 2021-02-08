651683643a3d8840c8bfe75f46554d400a0be180
Roger Lemos <rogerlemos28@gmail.com>
2021-02-08 20:57:18 -0300

    Posicionamento quase 100% acertado

--- interactive-pricing-component-main/script.js
+++ interactive-pricing-component-main/script.js
@@ -4,58 +4,113 @@ let slider = document.getElementById("myRange");
 let outputPrice = document.getElementById("demo");
 let outputViews = document.getElementById("pageviews");
 
-let oneThousandsViewsPrice = 6.25;
+let sliderValue = parseInt(slider.value)
+let viewsPrice = definePrice(sliderValue)
+let views = viewsPrice [0]
+let price = viewsPrice [1]
 
-
-let price = slider.value
-let views = oneThousandsViewsPrice  * price ;
-
-outputPrice.innerHTML =  "$ " + price   ;
-outputViews.innerHTML = 116.00   - price  ;
  
-let priceText = '$ '  + price  ;
-let viewsText = views + "K PAGEVIEWS";
+  sliderValue = parseInt(slider.value)
+
+
+outputPrice.innerHTML =  '$ '  + price.toFixed(2);
+outputViews.innerHTML = views;
+ 
+let priceText = '$ '  + price.toFixed(2);
+let viewsText = views;
  
  
 slider.oninput = 
 
 function () {
- 
+  sliderValue = parseInt(slider.value)
+
   if (switchValue.checked == true){   
     
-    price = this.value;
-    views = oneThousandsViewsPrice  * price ;
+    
+    viewsPrice = definePrice(sliderValue)
+    views = viewsPrice [0]
+    price = viewsPrice [1]
+    
     let discount = (price   * 25/100);
     price = price - discount;
-    priceText = '$ '  + price ;
-    viewsText = views + "K PAGEVIEWS" ;
-
-    outputViews.innerHTML = viewsText;
-    outputPrice.innerHTML = priceText ;
+    priceText = '$ '  + price.toFixed(2);
+    outputViews.innerHTML = views;
+    outputPrice.innerHTML = priceText;
   }
   else {
      
-    price = this.value ;
-    views = oneThousandsViewsPrice  * price ;
-    priceText = '$ '  + price ;
-    viewsText = views + "K PAGEVIEWS"
-
-
-    outputViews.innerHTML = viewsText;
+   
+    viewsPrice = definePrice(sliderValue)
+    views = viewsPrice [0]
+    price = viewsPrice [1]
+    priceText = '$ '  + price.toFixed(2); 
+    outputViews.innerHTML = views;
     outputPrice.innerHTML = priceText ;
   }
   
 }
+function definePrice(sliderValue){
+  var views 
+  var price
+ 
+  switch(sliderValue) {
+    
+    case 1:
+      views = 10 + "K PAGEVIEWS";
+     
+      price = 8.00;
+      break;
+    case 2:
+      views = 50 + "K PAGEVIEWS";
+      price = 12.00;
+      break;
+    case 3:
+      views =  100 + "K PAGEVIEWS";
+      price = 16.00;
+      break;
+    case 4:
+      views = 500 + "K PAGEVIEWS";
+      price = 24.00;
+      break;
+    case 5:
+      views = 1 + "M PAGEVIEWS";
+      price = 36.00;
+      break;
+    default:
+      console.log(sliderValue);
+      // code block
+    } 
+    return [views, price];
+    
+
+}
+
+
+
  
 switchValue.onchange =   function () {
+  sliderValue = parseInt(slider.value)
   if (switchValue.checked == true){
-    let discount = (slider.value  * 25/100);
-    let price = slider.value - discount;
-    outputPrice.innerHTML = price ;
+    
+   
+    viewsPrice = definePrice(sliderValue)
+    views = viewsPrice [0]
+    price = viewsPrice [1]
+    
+    let discount = (price   * 25/100);
+    price = price - discount;
+    priceText = '$ '  + price.toFixed(2);
+    outputViews.innerHTML = views;
+    outputPrice.innerHTML = priceText;
   }
   else {
-     
-    let price = slider.value;
-    outputPrice.innerHTML = price ;
+    viewsPrice = definePrice(sliderValue)
+    views = viewsPrice [0]
+    price = viewsPrice [1]
+    priceText = '$ '  + price.toFixed(2); 
+    outputViews.innerHTML = views;
+    outputPrice.innerHTML = priceText ;
   }
 }
+
